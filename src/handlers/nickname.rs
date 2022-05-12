91dc27130c67092364ffd6d45710c385369df7c0
Daniel Cherubini <daniel@cherubini.casa>
2022-05-12 16:21:03 +0200

    fixed a bug in the nick

--- src/handlers/nickname.rs
+++ src/handlers/nickname.rs
@@ -5,8 +5,13 @@ fn clean_username(nick: &String) -> String {
 // fix_nickname is a function to add the nickname for horny/phony
 pub fn fix_nickname(nick: &String, prefix: &String) -> String {
     // check if the nickname has the prefix in it
-    if nick.contains(" | ") {
+    let nick_to_find = format!("{} | ", prefix);
+    if nick.contains(&nick_to_find) {
+        // the prefix is already in the nick so just clean
         return clean_username(nick);
+    } else if nick.contains(" | ") {
+        // the prefix doesn't match, but there's a pipe in there
+        return format!("{} | {}", prefix, clean_username(nick));
     } else {
         return format!("{} | {}", prefix, nick);
     }
@@ -15,12 +20,27 @@ pub fn fix_nickname(nick: &String, prefix: &String) -> String {
 #[cfg(test)]
 mod tests {
     #[test]
-    fn nickname_addition() {
+    fn horny() {
         let nick = String::from("foo");
         let prefix = String::from("horny");
         let positive_test = super::fix_nickname(&nick, &prefix);
-        assert_eq!(positive_test, format!("{} | {}", prefix, nick));
+        assert_eq!(positive_test, String::from("horny | foo"));
     }
+    #[test]
+    fn phony() {
+        let nick = String::from("foo");
+        let prefix = String::from("phony");
+        let positive_test = super::fix_nickname(&nick, &prefix);
+        assert_eq!(positive_test, String::from("phony | foo"));
+    }
+    #[test]
+    fn swap() {
+        let nick = String::from("horny | foo");
+        let prefix = String::from("phony");
+        let positive_test = super::fix_nickname(&nick, &prefix);
+        assert_eq!(positive_test, String::from("phony | foo"));
+    }
+
     #[test]
     fn nickname_clean_one() {
         let nick = String::from("horny | foo");
