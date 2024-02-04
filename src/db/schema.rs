4fc7959d3b7570848fd7bca12e74ba34be9c36b1
Daniel Cherubini <daniel.cherubini@Daniels-MBP.cherub.casa>
2024-02-04 22:07:11 +0100

    adding schema changes and testing gitlab-ci changes

--- src/db/schema.rs
+++ src/db/schema.rs
@@ -1,9 +1,7 @@
 // @generated automatically by Diesel CLI.
 
 pub mod sql_types {
-    use diesel::QueryId;
-
-    #[derive(diesel::sql_types::SqlType, QueryId)]
+    #[derive(diesel::query_builder::QueryId, diesel::sql_types::SqlType)]
     #[diesel(postgres_type(name = "job_status"))]
     pub struct JobStatus;
 }
