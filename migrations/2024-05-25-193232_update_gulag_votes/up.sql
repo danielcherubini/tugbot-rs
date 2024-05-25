22747f07e08823695e158aca324efaee2e6ae809
Daniel Cherubini <daniel@cherubini.casa>
2024-05-25 22:36:49 +0200

    updates diesel migration

--- migrations/2024-05-25-193232_update_gulag_votes/up.sql
+++ migrations/2024-05-25-193232_update_gulag_votes/up.sql
@@ -3,3 +3,7 @@ ALTER TABLE "message_votes"
 
 ALTER TABLE "message_votes"
   ADD COLUMN total_vote_tally int NOT NULL DEFAULT 0;
+
+UPDATE "message_votes"
+  SET total_vote_tally = current_vote_tally, current_vote_tally = 0
+  WHERE current_vote_tally > 0 AND (current_vote_tally % 5) = 0;
