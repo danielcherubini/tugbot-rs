01fc237546699a42c0097045a0539e7c429fa3ef
Daniel Cherubini <daniel@cherubini.casa>
2024-09-10 18:51:59 +0200

    committing to stash

--- src/handlers/tiktok.rs
+++ src/handlers/tiktok.rs
@@ -1,12 +1,13 @@
 use regex::Regex;
+use reqwest::Result;
 use serenity::{model::channel::Message, prelude::Context};
 
 pub struct TikTok;
 
 impl TikTok {
     pub async fn handler(ctx: &Context, msg: &Message) {
-        match Self::fx_rewriter(&msg.content.to_owned()) {
-            None => return,
+        match Self::fx_rewriter(&msg.content.to_owned()).await {
+            None => (),
             Some(fixed_message) => {
                 if let Err(why) = msg.to_owned().suppress_embeds(ctx.to_owned()).await {
                     println!("Error supressing embeds {:?}", why);
@@ -22,19 +23,27 @@ impl TikTok {
         }
     }
 
-    fn fx_rewriter(url: &str) -> Option<String> {
+    async fn fx_rewriter(url: &str) -> Option<String> {
         let re = Regex::new(r"https://((www.)?tiktok.com)/.+").unwrap();
 
-        match re.captures(&url) {
+        match re.captures(url) {
             None => None,
             Some(caps) => match caps.get(0) {
                 None => None,
-                Some(full) => match caps.get(1) {
-                    None => None,
-                    Some(short) => Some(full.as_str().replace(short.as_str(), "vm.offtiktok.com")),
+                Some(full) => match Self::get_url(full.as_str()).await {
+                    Ok(off_tiktok) => Some(off_tiktok),
+                    Err(_e) => None,
                 },
             },
         }
+    }
+
+    async fn get_url(url: &str) -> Result<String> {
+        let base_url = "https://offtiktok.com/api/by_id/".to_owned();
+        let full_url = base_url + url;
+        let response = reqwest::get(full_url).await?.text().await;
+        println!("{:?}", response);
+        Ok("foo".to_string())
     }
 }
 
@@ -42,17 +51,14 @@ impl TikTok {
 mod tests {
     use super::TikTok;
 
-    #[test]
-    fn rewrite() {
-        match TikTok::fx_rewriter("https://tiktok.com/t/7m4Kxl") {
-            None => assert!(false),
-            Some(url) => assert_eq!(url, "https://vm.offtiktok.com/t/7m4Kxl",),
-        }
-    }
-    #[test]
-    fn with_www() {
-        match TikTok::fx_rewriter("https://www.tiktok.com/t/7m4Kxl") {
-            None => assert!(false),
+    #[tokio::test]
+    async fn rewrite() {
+        match TikTok::fx_rewriter(
+            "https://www.tiktok.com/@centralparkturtle/video/7412424505374674207",
+        )
+        .await
+        {
+            None => panic!(),
             Some(url) => assert_eq!(url, "https://vm.offtiktok.com/t/7m4Kxl",),
         }
     }
