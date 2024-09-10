5a5303944b094a775eafee4ce883f16c0071e1d0
Daniel Cherubini <daniel@cherubini.casa>
2024-09-10 20:02:45 +0200

    fixing up offtiktok (#2)
    * committing to stash
    
    * finished
    
    * bumping version
    
    * bumping rust version
    
    * changing workflows
    
    * testing something
    
    * updates test
    
    * changes the deploy workflow
    
    * changes to work only on published releases
    
    * updates workflows
    
    * changing to rust 1.81

--- src/handlers/tiktok.rs
+++ src/handlers/tiktok.rs
@@ -1,12 +1,13 @@
 use regex::Regex;
+use reqwest::Result;
 use serenity::{model::channel::Message, prelude::Context};
 
 pub struct TikTok;
 
 impl TikTok {
     pub async fn handler(ctx: &Context, msg: &Message) {
-        match Self::fx_rewriter(&msg.content.to_owned()) {
-            None => return,
+        match Self::fx_rewriter(&msg.content.to_owned()).await {
+            None => (),
             Some(fixed_message) => {
                 if let Err(why) = msg.to_owned().suppress_embeds(ctx.to_owned()).await {
                     println!("Error supressing embeds {:?}", why);
@@ -22,19 +23,30 @@ impl TikTok {
         }
     }
 
-    fn fx_rewriter(url: &str) -> Option<String> {
+    async fn fx_rewriter(url: &str) -> Option<String> {
         let re = Regex::new(r"https://((www.)?tiktok.com)/.+").unwrap();
-
-        match re.captures(&url) {
+        let re_redirect = Regex::new(r"NEXT_REDIRECT;replace;(/post/\d+);").unwrap();
+        match re.captures(url) {
             None => None,
             Some(caps) => match caps.get(0) {
                 None => None,
-                Some(full) => match caps.get(1) {
-                    None => None,
-                    Some(short) => Some(full.as_str().replace(short.as_str(), "vm.offtiktok.com")),
+                Some(full) => match Self::get_url(full.as_str()).await {
+                    Ok(off_tiktok) => match re_redirect.captures(&off_tiktok) {
+                        Some(final_url_caps) => final_url_caps
+                            .get(1)
+                            .map(|f| "https://offtiktok.com".to_owned() + f.as_str()),
+                        None => None,
+                    },
+                    Err(_e) => None,
                 },
             },
         }
+    }
+
+    async fn get_url(url: &str) -> Result<String> {
+        let base_url = "https://offtiktok.com/api/by_url/".to_owned();
+        let full_url = base_url + url;
+        reqwest::get(full_url).await?.text().await
     }
 }
 
@@ -42,18 +54,15 @@ impl TikTok {
 mod tests {
     use super::TikTok;
 
-    #[test]
-    fn rewrite() {
-        match TikTok::fx_rewriter("https://tiktok.com/t/7m4Kxl") {
-            None => assert!(false),
-            Some(url) => assert_eq!(url, "https://vm.offtiktok.com/t/7m4Kxl",),
-        }
-    }
-    #[test]
-    fn with_www() {
-        match TikTok::fx_rewriter("https://www.tiktok.com/t/7m4Kxl") {
-            None => assert!(false),
-            Some(url) => assert_eq!(url, "https://vm.offtiktok.com/t/7m4Kxl",),
+    #[tokio::test]
+    async fn rewrite() {
+        match TikTok::fx_rewriter(
+            "https://www.tiktok.com/@centralparkturtle/video/7412424505374674207",
+        )
+        .await
+        {
+            None => panic!(),
+            Some(url) => assert_eq!(url, "https://offtiktok.com/post/6760",),
         }
     }
 }
