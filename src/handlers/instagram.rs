0e2e23cd572b0339b9aee2f1d2e8aa5cecdbc36c
Daniel Cherubini <daniel@cherubini.casa>
2024-11-21 16:43:45 +0100

    Added instagram (#19)
    * Added instagram
    
    * updates regex

---
+++ src/handlers/instagram.rs
@@ -0,0 +1,60 @@
+use regex::Regex;
+use serenity::{model::channel::Message, prelude::Context};
+
+use crate::features::Features;
+
+pub struct Instagram;
+
+impl Instagram {
+    pub async fn handler(ctx: &Context, msg: &Message) {
+        if Features::is_enabled("instagram".to_string()) {
+            match Self::fx_rewriter(&msg.content.to_owned()) {
+                None => (),
+                Some(fixed_message) => {
+                    if let Err(why) = msg.to_owned().suppress_embeds(ctx.to_owned()).await {
+                        println!("Error supressing embeds {:?}", why);
+                    }
+
+                    println!("Suppressed Embed");
+                    if let Err(why) = msg.channel_id.say(ctx, fixed_message).await {
+                        println!("Error Editing Message to Tweet {:?}", why);
+                    }
+
+                    println!("Posted Tweet");
+                }
+            }
+        }
+    }
+
+    fn fx_rewriter(url: &str) -> Option<String> {
+        let re = Regex::new(r"https://(www\.)?(instagram\.com)/.+").unwrap();
+
+        match re.captures(url) {
+            None => None,
+            Some(caps) => match caps.get(0) {
+                None => None,
+                Some(full) => caps
+                    .get(2)
+                    .map(|short| full.as_str().replace(short.as_str(), "ddinstagram.com")),
+            },
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Instagram;
+
+    #[test]
+    fn instagram_rewrite() {
+        match Instagram::fx_rewriter(
+            "https://www.instagram.com/reel/DCkUQSry42v/?igsh=MXNrMDFwbTEzZnFvMg==",
+        ) {
+            None => panic!(),
+            Some(url) => assert_eq!(
+                url,
+                "https://www.ddinstagram.com/reel/DCkUQSry42v/?igsh=MXNrMDFwbTEzZnFvMg==",
+            ),
+        }
+    }
+}
