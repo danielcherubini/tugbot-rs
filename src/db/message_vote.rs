9216c88e93399949b87f7ca7539f8f10318f40c2
Daniel Cherubini <daniel@cherubini.casa>
2025-02-11 18:52:34 +0100

    fixing issue

--- src/db/message_vote.rs
+++ src/db/message_vote.rs
@@ -40,7 +40,7 @@ impl MessageVoteHandler {
                     Err(anyhow!("You have already Voted"))
                 } else {
                     message.voters.push(Some(voter_id as i64));
-                    current_vote_tally = message.current_vote_tally + 1;
+                    let current_vote_tally: i32 = message.current_vote_tally + 1;
                     match diesel::update(message_votes::dsl::message_votes.find(message_id as i64))
                         .set((
                             message_votes::current_vote_tally.eq(current_vote_tally),
