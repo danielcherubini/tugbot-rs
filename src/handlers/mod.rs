853d90fd754f55def983247492923450d162e12a
Daniel Cherubini <daniel@cherubini.casa>
2026-05-24 22:29:35 +0200

    fix: remove broken intents debug log

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -220,7 +220,7 @@ impl EventHandler for Handler {
     }
 
     async fn ready(&self, ctx: Context, ready: Ready) {
-        println!("{} is connected! intents={:?}", ready.user.name, ready.intents);
+        println!("{} is connected!", ready.user.name);
 
         let pool = get_pool(&ctx).await;
 
